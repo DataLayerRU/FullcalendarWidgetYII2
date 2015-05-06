d727b186c3021374dbc3d2bf42c78157bf973a72
s.zinchenko <s.zinchenko@rassvet.ru>
2015-05-06 16:59:34 +0300

    Dependency 'moment' is added; Some fixes

--- assets/FullcalendarAsset.php
+++ assets/FullcalendarAsset.php
@@ -6,14 +6,17 @@ use yii\web\AssetBundle;
 
 class FullcalendarAsset extends AssetBundle
 {
-    public $sourcePath = __DIR__.'/fullcalendar-2.3.1';
+    public $sourcePath = '@vendor/datalayerru/fullcalendar-widget-yii2/assets/fullcalendar-2.3.1';
     public $js         = [
         'fullcalendar.min.js',
         'gcal.js',
         'lang-all.js'
     ];
-    public $css = [
+    public $css        = [
         'fullcalendar.min.css'
+    ];
+    public $depends    = [
+        'yii\web\YiiAsset'
     ];
 
 }
